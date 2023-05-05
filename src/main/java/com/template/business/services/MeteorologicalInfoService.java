9155e209f83735a54862404df6307c77f6a3d3e3
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-05-05 11:10:03 -0300

    ✨feat: created a getAll service

--- src/main/java/com/template/business/services/MeteorologicalInfoService.java
+++ src/main/java/com/template/business/services/MeteorologicalInfoService.java
@@ -1,14 +1,21 @@
 package com.template.business.services;
 
+import com.template.data.DTO.MeteorologicalInfoDTO;
 import com.template.data.entity.MeteorologicalInfoEntity;
 import com.template.data.repository.MeteorologicalInfoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 @Service
 public class MeteorologicalInfoService {
-
 
     private MeteorologicalInfoRepository repository;
 
@@ -17,8 +24,23 @@ public class MeteorologicalInfoService {
         this.repository = repository;
     }
 
-    public MeteorologicalInfoEntity create(@RequestBody MeteorologicalInfoEntity meteorologicalInfoEntity){
-        return repository.save(meteorologicalInfoEntity);
+    public Page<MeteorologicalInfoEntity> getAll(Pageable pagination){
+        return repository.findAll(pagination);
+    }
+
+    @Transactional
+    public MeteorologicalInfoEntity create(@RequestBody MeteorologicalInfoDTO metInfoDTO){
+        return repository.save(new MeteorologicalInfoEntity(
+                metInfoDTO.city(),
+                metInfoDTO.weatherDate(),
+                metInfoDTO.morningWeather(),
+                metInfoDTO.nightWeather(),
+                metInfoDTO.maxTemperature(),
+                metInfoDTO.minTemperature(),
+                metInfoDTO.precipitaion(),
+                metInfoDTO.humidity(),
+                metInfoDTO.windSpeed()
+                ));
     }
 
 }
