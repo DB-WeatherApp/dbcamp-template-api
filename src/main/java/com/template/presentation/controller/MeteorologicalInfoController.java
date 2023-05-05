b51bdc1e935c119237fa30e9709d95d0d84c820a
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-05-05 11:13:51 -0300

    ✨feat: creating a getall method in controller

--- src/main/java/com/template/presentation/controller/MeteorologicalInfoController.java
+++ src/main/java/com/template/presentation/controller/MeteorologicalInfoController.java
@@ -1,8 +1,13 @@
 package com.template.presentation.controller;
 
 import com.template.business.services.MeteorologicalInfoService;
+import com.template.data.DTO.MeteorologicalInfoDTO;
 import com.template.data.entity.MeteorologicalInfoEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,20 +18,38 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4767")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/meteorologicalInfo")
 public class MeteorologicalInfoController {
 
     @Autowired
     MeteorologicalInfoService service;
 
-    @PostMapping("/meteorologicalInfo")
-    public ResponseEntity<MeteorologicalInfoEntity> createTutorial(@RequestBody MeteorologicalInfoEntity meteorologicalInfo) {
+    @PostMapping
+    public ResponseEntity<MeteorologicalInfoEntity> createMeteorologicalData(@RequestBody MeteorologicalInfoDTO meteorologicalInfoDTO) {
         try {
-            MeteorologicalInfoEntity metInfo = service.create(meteorologicalInfo);
-            return new ResponseEntity<>(metInfo, HttpStatus.CREATED);
+            MeteorologicalInfoEntity metInfo = service.create(meteorologicalInfoDTO);
+            return new ResponseEntity<MeteorologicalInfoEntity>(metInfo, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    @GetMapping
+    public ResponseEntity<Page<MeteorologicalInfoEntity>> getAllMeteorologicalInfo(@PageableDefault(
+            size=10,
+            sort = {"weatherDate"},
+            direction = Sort.Direction.DESC
+    )
+            Pageable page
+    ){
+        try{
+            Page<MeteorologicalInfoEntity> metInfo = service.getAll(page);
+            if (metInfo.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(metInfo,HttpStatus.OK);
+        }catch (Exception error){
+                return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
